7d8bf7f1b09d678c9f52d5393680e81ff3dd59eb
hubiao <hubiao@qq.com>
2020-06-22 11:34:25 +0800

    修改文件目录架构

--- Community/Classes/Login/View/UnderlineText.m
+++ Community/Classes/Login/View/UnderlineText.m
@@ -18,18 +18,12 @@
 //    CGContextStrokePath(context);
     
     CGContextRef context = UIGraphicsGetCurrentContext();
-
     [RGBACOLOR(201, 201, 201, 1) set];//设置下划线颜色 这里是红色 可以自定义
-
     CGFloat y = CGRectGetHeight(self.frame);
-
     CGContextMoveToPoint(context, 0, y);
-
     CGContextAddLineToPoint(context, CGRectGetWidth(self.frame), y);
-
     //设置线的宽度
     CGContextSetLineWidth(context, 1);
-
     //渲染 显示到self上
     CGContextStrokePath(context);
 }
